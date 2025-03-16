a71e7b1c6dca108e00f75558d406692c126f7409
Jiawen Geng <technicalcute@gmail.com>
2025-03-16 10:41:41 +0000

    fix: build

--- src/app/blitz-server.ts
+++ src/app/blitz-server.ts
@@ -5,16 +5,17 @@ import { BlitzLogger } from "blitz"
 import { RpcServerPlugin } from "@blitzjs/rpc"
 import { authConfig } from "./blitz-auth-config"
 
-const { api, getBlitzContext, useAuthenticatedBlitzContext, invoke } = setupBlitzServer({
-  plugins: [
-    AuthServerPlugin({
-      ...authConfig,
-      storage: PrismaStorage(db),
-      isAuthorized: simpleRolesIsAuthorized,
-    }),
-    RpcServerPlugin({}),
-  ],
-  logger: BlitzLogger({}),
-})
+const { api, getBlitzContext, useAuthenticatedBlitzContext, invoke, withBlitzAuth } =
+  setupBlitzServer({
+    plugins: [
+      AuthServerPlugin({
+        ...authConfig,
+        storage: PrismaStorage(db),
+        isAuthorized: simpleRolesIsAuthorized,
+      }),
+      RpcServerPlugin({}),
+    ],
+    logger: BlitzLogger({}),
+  })
 
-export { api, getBlitzContext, useAuthenticatedBlitzContext, invoke }
+export { api, getBlitzContext, useAuthenticatedBlitzContext, invoke, withBlitzAuth }
